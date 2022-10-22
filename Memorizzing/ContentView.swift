6b0e4673916de8ff0f57d7ea906ae82477016ffa
Sixeautiful <vladerastatkevich2015@gmail.com>
2022-10-22 21:30:05 +0200

    Initial Commit

---
+++ Memorizzing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorizzing
+//
+//  Created by Kirill Willer on 22.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorizzing/MemorizzingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizzingApp.swift
+//  Memorizzing
+//
+//  Created by Kirill Willer on 22.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizzingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
